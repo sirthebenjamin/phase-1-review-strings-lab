1866e23dc4902e041c79809c3424f82cdf843fad
sirthebenjamin <126521851+sirthebenjamin@users.noreply.github.com>
2023-03-29 19:21:34 -0400

    Adss myfile.md

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code in this file!
+const currentUser = 'Grace Hopper';
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const firstInitial = currentUser[0];
+const shortGreeting = 'Welcome, ' + firstInitial + '!';
